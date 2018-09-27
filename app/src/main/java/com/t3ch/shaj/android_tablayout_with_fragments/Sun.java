1bc6dba880c4193e3dd6eccd920cbc4ec22978b9
SHAJ <shakilahmedshaj@gmail.com>
2018-09-27 17:36:21 +0600

    Initial commit

---
+++ app/src/main/java/com/t3ch/shaj/android_tablayout_with_fragments/Sun.java
@@ -0,0 +1,21 @@
+package com.t3ch.shaj.android_tablayout_with_fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Shakil Ahmed Shaj on 27-Sep-18.
+ */
+public class Sun extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.sun, container, false);
+    }
+}
